686c423c653af1a5fefa87a14edaf539479e6445
coopcode <ronpatiro@gmail.com>
2012-06-11 11:26:19 -0400

    Sample app after lesson 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,59 @@ describe "Static pages" do
   
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-    
-    
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
   
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-    
-    
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
   
-
   describe "About page" do
 
-    it "should have the content 'About us'" do
-    
-     
+    it "should have the h1 'About us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+describe "Contact Page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
