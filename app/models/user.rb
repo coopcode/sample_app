689088a047269ecd19a9ddb5db3c9c0d9b47f4d3
coopcode <ronpatiro@gmail.com>
2012-06-19 17:44:22 -0400

    finish sign in - still cant sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation 
   has_secure_password
 
-  before_save { |user| user.email = user.email.downcase }
+  before_save { |user| user.email = email.downcase }
   before_save :create_remember_token 
 
   validates :name,  presence: true, length: { maximum: 50 }
